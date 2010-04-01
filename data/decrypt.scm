9cdb9c856ec75d431f08f2b7a55b2711b1895dd9
Tatsuya BIZENN <bizenn@kahua.org>
2010-04-01 21:34:19 +0900

    Add sample data and program(working in progress).

---
+++ data/decrypt.scm
@@ -0,0 +1,37 @@
+#!/usr/bin/env gosh
+;;; -*- mode: scheme; coding: utf-8 -*-
+
+(use rfc.base64)
+(use rfc.md5)
+(use file.util)
+(use gauche.uvector)
+(use gauche.charconv)
+(use org.visha.crypt.mcrypt)
+
+;; Compatibility with Crypt::CBC
+(define-constant *IV* (string->u8vector "$KJh#(}q"))
+(define-constant *KEY* (file->string "Key.txt"))
+(define-constant *KEY-LENGTH* 56)
+(define (regenerate-key original-key key-size)
+  (let loop ((material (md5-digest-string original-key)))
+    (if (>= (string-size material) key-size)
+	(substring material 0 key-size)
+	(loop (string-append material (md5-digest-string material))))))
+
+(define (with-input-decryption-from-base64)
+  (decrypt-string (make-mcrypt BLOWFISH CBC)
+		  (with-output-to-string base64-decode)
+		  (regenerate-key *KEY* *KEY-LENGTH*) :padding :space :iv *IV*))
+
+(define (blowfish&base64-file->plain-text-string fname)
+  (with-input-from-file fname
+    (lambda ()
+      (call-with-input-string (with-input-decryption-from-base64)
+	(lambda (in)
+	  (with-input-conversion in
+	    (lambda ()
+	      (call-with-output-string (cut copy-port (current-input-port) <>)))
+	    :encoding "ISO-2022-JP"))))))
+
+(define (main args)
+  0)
